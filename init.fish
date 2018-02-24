3dd68506977b8bff987de897c30d29a7af3df116
anymuse <anymuse@gmail.com>
2018-02-24 14:31:39 +0800

    Add global variables.

--- init.fish
+++ init.fish
@@ -1,2 +1,5 @@
 set -gx PATH $HOME/.rbenv/shims $PATH
+set -gx PATH $HOME/depot_tools $PATH
 set -gx PYTHONDONTWRITEBYTECODE 1
+set -gx http_proxy http://127.0.0.1:1087
+set -gx https_proxy http://127.0.0.1:1087
