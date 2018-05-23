a435c0d19a333acc0c5b339a733d92542457723c
anymuse <anymuse@gmail.com>
2018-05-23 20:34:37 +0800

    Add python bin to PATH.

--- init.fish
+++ init.fish
@@ -1,5 +1,6 @@
 set -gx PATH $HOME/.rbenv/shims $PATH
 set -gx PATH $HOME/depot_tools $PATH
+set -gx PATH $HOME/Library/Python/3.6/bin $PATH
 set -gx PYTHONDONTWRITEBYTECODE 1
 set -gx http_proxy http://127.0.0.1:1087
 set -gx https_proxy http://127.0.0.1:1087
